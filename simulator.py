ba0f5abd20c3835d6e307f395562ab4c5adcd354
= <=>
2021-09-15 17:16:11 -0500

    first

---
+++ simulator.py
@@ -0,0 +1,21 @@
+"""
+Contains industry name, growth factor
+"""
+class Industry:
+    def __init__(self) -> None:
+        pass
+
+"""
+Overall growth factor
+"""
+class Stock:
+    def __init__(self):
+        self.data = []
+
+
+"""
+Maps stocks to price timeseries
+"""
+class MarketData:
+    def __init__(self) -> None:
+        pass
